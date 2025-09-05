242867e3dabf340805a6063582144becd5b2b1bf
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-05 15:17:10 +0545

    cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.postimg.cc",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
